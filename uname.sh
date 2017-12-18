42956d2f1b1eaecff91b798b6aa84ce75885f044
Shivam Bansal <cs1170376@iitd.ac.in>
2017-12-18 10:28:37 +0530

    add different error checking cases

--- uname.sh
+++ uname.sh
@@ -7,7 +7,16 @@ if [ -z "$2" ]
 then
 	exit 1;
 fi
-
+tester=`echo $1 | grep ^..*\.txt$ | wc -l`
+if [ $tester -ne 1 ]
+then
+	exit 1
+fi
+tmp=`cat $1 2>/dev/null`
+if [ $? -ne 0 ]
+then
+	exit 1
+fi
 ifs_old="$IFS"
 IFS=:
 search=`cat $1 | grep ^$2: | cut -d: -f5 `
